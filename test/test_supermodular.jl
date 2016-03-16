45088cbd708ea80e43fc73d27ed23d68f4cb127f
kve216 <kve216@nyu.edu>
2016-03-16 17:29:39 -0400

    Updated test for is_supermodular

--- test/test_supermodular.jl
+++ test/test_supermodular.jl
@@ -1,8 +1,17 @@
-# Test script for supermodular games
+using Games
+
+if VERSION >= v"0.5-"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
+
+# Test script for supermodular
 
 @testset "Testing supermodular.jl" begin
     
-    @testset "is_supermodular for Player" 
+    @testset "is_supermodular for Player" begin 
         p1 = Player(zeros((2, 3)))
         coordination_game_matrix = [4 0; 3 2]
     	p2 = Player(coordination_game_matrix)
