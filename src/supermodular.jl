8f595901b711202b9acad0ebd32643383a40e438
kve216 <kve216@nyu.edu>
2016-03-16 16:56:24 -0400

    Updated the is_supermodular name

--- src/supermodular.jl
+++ src/supermodular.jl
@@ -1,6 +1,6 @@
 # Katrina Evtimova, kve216
 
-function is_supermodular_p(player::Player{2})
+function is_supermodular(player::Player{2})
     row_diff = diff(player.payoff_matrix, 1)
     col_diff = diff(row_diff, 2)
     return all(col_diff .>= 0)
