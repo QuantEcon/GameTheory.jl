d30837836f77c60ec3c92a6de2fc2b799487106d
kve216 <kve216@nyu.edu>
2016-03-16 16:55:38 -0400

    Added is_supermodular

--- src/Games.jl
+++ src/Games.jl
@@ -2,9 +2,11 @@ module Games
 
 # package code goes here
 include("normal_form_game.jl")
+include("supermodular.jl")
 
 export Player, NormalFormGame,
  	   best_response, best_responses, is_best_response, payoff_vector,
-       is_nash, pure2mixed, num_players, num_actions, num_opponents
+       is_nash, pure2mixed, num_players, num_actions, num_opponents,
+       is_supermodular
 
 end # module
