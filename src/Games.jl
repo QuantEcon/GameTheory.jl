04c20f2aee61da452c5179fb1f4d4ab3007ad08a
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2017-03-29 11:24:09 +0900

    Correct indentations

--- src/Games.jl
+++ src/Games.jl
@@ -24,25 +24,27 @@ include("repeated_game_util.jl")
 include("repeated_game.jl")
 
 
-export Player, NormalFormGame,  # Types
+export
+    # Types
+    Player, NormalFormGame,
 
-       # Type aliases
-       Action, MixedAction, PureAction, ActionProfile,
+    # Type aliases
+    Action, MixedAction, PureAction, ActionProfile,
 
-       # Normal form game functions
- 	   best_response, best_responses, is_best_response, payoff_vector,
-       is_nash, pure2mixed, pure_strategy_NE,
+    # Normal form game functions
+    best_response, best_responses, is_best_response, payoff_vector,
+    is_nash, pure2mixed, pure_strategy_NE,
 
-       # General functions
-       num_players, num_actions, num_opponents,
+    # General functions
+    num_players, num_actions, num_opponents,
 
-       # Nash Equilibrium
-       pure_nash,
+    # Nash Equilibrium
+    pure_nash,
 
-       # Repeated Games
-       RepeatedGame, unpack, flow_u_1, flow_u_2, flow_u, best_dev_i,
-       best_dev_1, best_dev_2, best_dev_payoff_i, best_dev_payoff_1,
-       best_dev_payoff_2, worst_value_i, worst_value_1, worst_value_2,
-       worst_values, outerapproximation
+    # Repeated Games
+    RepeatedGame, unpack, flow_u_1, flow_u_2, flow_u, best_dev_i,
+    best_dev_1, best_dev_2, best_dev_payoff_i, best_dev_payoff_1,
+    best_dev_payoff_2, worst_value_i, worst_value_1, worst_value_2,
+    worst_values, outerapproximation
 
 end # module
