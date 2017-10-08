d2f7a92f5fbba4ab4b2976072df1e410400f886f
QBatista <quentin.batista@mail.mcgill.ca>
2017-10-08 15:31:55 +0900

    RFC: Move the unitcircle function to repeated_game

--- src/repeated_game.jl
+++ src/repeated_game.jl
@@ -81,6 +81,28 @@ best_dev_payoff_2(rpd::RepGame2, a1::Int) =
     maximum(rpd.sg.players[2].payoff_array[:, a1])
 
 """
+    unitcircle(npts)
+
+Places `npts` equally spaced points along the 2 dimensional circle and returns 
+the points with x coordinates in first column and y coordinates in second
+column.
+"""
+function unitcircle(npts::Int)
+    # Want our points placed on [0, 2π]
+    incr = 2π / npts
+    degrees = 0.0:incr:2π
+
+    # Points on circle
+    pts = Array{Float64}(npts, 2)
+    for i=1:npts
+        x = degrees[i]
+        pts[i, 1] = cos(x)
+        pts[i, 2] = sin(x)
+    end
+    return pts
+end
+
+"""
     initialize_sg_hpl(nH, o, r)
 
 Initializes subgradients, extreme points and hyperplane levels for the 
