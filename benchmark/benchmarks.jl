ac8d4bf526dc77cf8b4f8835027c8e936fc2a068
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2018-03-04 13:59:13 +0900

    BENCH: Add benchmarks for bimatrix_generators

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,4 +1,5 @@
 using Games
+using Games.Generators
 using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
@@ -22,4 +23,41 @@ for n in ns
     g = NormalFormGame(eye(T, n))
     SUITE["support_enumeration"]["Rational"][sz] =
         @benchmarkable support_enumeration($g)
+end
+
+
+SUITE["bimatrix_generators"] = BenchmarkGroup(["bimatrix_generators"])
+seed = 0
+rng = MersenneTwister(seed)
+
+# blotto_game
+hts = [(3, 62), (4, 21)]  # (h, t)
+rho = 0.5
+SUITE["bimatrix_generators"]["blotto_game"] = BenchmarkGroup()
+for ht in hts
+    SUITE["bimatrix_generators"]["blotto_game"][ht] =
+        @benchmarkable blotto_game($rng, ($ht)..., $rho)
+end
+
+# ranking_game
+n = 2000
+SUITE["bimatrix_generators"]["ranking_game"] =
+    @benchmarkable ranking_game($rng, $n)
+
+# sgc_game
+k = 500
+SUITE["bimatrix_generators"]["sgc_game"] = @benchmarkable sgc_game($k)
+
+# tournament_game
+n, k = 200, 2
+SUITE["bimatrix_generators"]["tournament_game"] =
+    @benchmarkable tournament_game($seed, $n, $k)
+
+# unit_vector_game
+n = 2000
+bools = [true, false]
+SUITE["bimatrix_generators"]["unit_vector_game"] = BenchmarkGroup()
+for b in bools
+    SUITE["bimatrix_generators"]["unit_vector_game"][b] =
+        @benchmarkable unit_vector_game($rng, $n; random=$b)
 end
