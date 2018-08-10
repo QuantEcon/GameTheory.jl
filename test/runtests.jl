a2f2c91e2459d335ff831f473ba260493afcc909
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2018-08-10 15:06:48 +0900

    TST: Skip test_repeated_game.jl for v0.7
    Too slow to run

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,11 @@ using Games
 using Base.Test
 
 include("test_pure_nash.jl")
-include("test_repeated_game.jl")
+if VERSION >= v"0.7-"
+	@test_skip include("test_repeated_game.jl")
+else
+	include("test_repeated_game.jl")
+end
 include("test_normal_form_game.jl")
 include("test_random.jl")
 include("test_support_enumeration.jl")
