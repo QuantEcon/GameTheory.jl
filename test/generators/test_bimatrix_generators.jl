9d76a49abe6bca12f995179441b9c2e1a8ae0278
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2018-08-27 22:33:34 +0900

    COMPAT: Add `dims=`

--- test/generators/test_bimatrix_generators.jl
+++ test/generators/test_bimatrix_generators.jl
@@ -106,7 +106,7 @@ import Combinatorics: binomial
 
             max_num_dominated_subsets = sum([binomial(i, k) for i in k:(n-1)])
             @test sum(g.players[1].payoff_array) <= max_num_dominated_subsets
-            @test all(sum(g.players[2].payoff_array, 2) .== k)
+            @test all(Compat.sum(g.players[2].payoff_array, dims=2) .== k)
         end
 
         @testset "test_seed" begin
@@ -134,7 +134,7 @@ import Combinatorics: binomial
         end
 
         @testset "test_payoff_values" begin
-            @test all(sum(g.players[1].payoff_array, 1) .== 1.)
+            @test all(Compat.sum(g.players[1].payoff_array, dims=1) .== 1.)
         end
 
         @testset "test_avoid_pure_nash" begin
