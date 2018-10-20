5c959a29c7f85c4dafeb7f913681654d1650cbf8
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2018-10-20 13:33:40 +0900

    MAINT: import -> using

--- src/generators/Generators.jl
+++ src/generators/Generators.jl
@@ -2,7 +2,7 @@ module Generators
 
 using Random
 
-import Games: Player, NormalFormGame
+using Games: Player, NormalFormGame
 
 include("bimatrix_generators.jl")
 export blotto_game, ranking_game, sgc_game, unit_vector_game, tournament_game
