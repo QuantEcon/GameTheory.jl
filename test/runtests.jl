2a63d73d894c3350666cc0d0bb0eb032f22e7b51
Sam Wycherley <samvpwycherley@gmail.com>
2022-06-04 08:19:19 +0100

    Include vertex enumeration tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,5 +7,7 @@ include("test_normal_form_game.jl")
 include("test_random.jl")
 include("test_support_enumeration.jl")
 include("test_lrsnash.jl")
+include("test_vertex_enumeration.jl")
+
 
 include("generators/runtests.jl")
