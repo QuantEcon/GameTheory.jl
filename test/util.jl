031060459eafd6f4d72b97839b5d656f51ad4af0
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2025-10-28 22:37:43 +0900

    BUG: Fix `isapprox_vecs_act_profs` in test/util.jl (#203)

--- test/util.jl
+++ test/util.jl
@@ -8,7 +8,7 @@ end
 
 function isapprox_vecs_act_profs(vec_act_profs1, vec_act_profs2)
     length(vec_act_profs1) == length(vec_act_profs2) || return false
-    for act_prof in vec_act_profs1
+    for act_prof in vec_act_profs2
         any(x -> isapprox_act_profs(x, act_prof),
             vec_act_profs1) || return false
     end
