98b161c61215fc153b59f6510c1f9cb85ebe5f4f
Zachary Sunberg <zachary.sunberg@colorado.edu>
2026-04-22 12:52:27 -0600

    return payoff profiles as SVector instead of Vector
    Eliminates per-cell heap allocation when showing a NormalFormGame. On a
    100x100 two-player game, show goes from ~3.5 s / 804 MiB to ~170 ms /
    130 MiB.

--- src/normal_form_game.jl
+++ src/normal_form_game.jl
@@ -712,17 +712,17 @@ end
 """
 LazyProfileArray
 
-Construct a lazily-evaluated array of payoff profiles. Each profile is a Vector that is created when getindex is called.
+Construct a lazily-evaluated array of payoff profiles. Each profile is an SVector that is created when getindex is called.
 
 This is meant to aid in printing without allocating large arrays.
 """
-struct LazyProfileArray{N,T} <: AbstractArray{Vector{T},N}
+struct LazyProfileArray{N,T} <: AbstractArray{SVector{N,T},N}
     g::NormalFormGame{N,T}
 end
 
 Base.size(a::LazyProfileArray) = a.g.nums_actions
 Base.getindex(a::LazyProfileArray{N}, index::Vararg{Int,N}) where {N} = a.g[index...]
-Base.getindex(a::LazyProfileArray{1}, index::Int) = [a.g[index]]
+Base.getindex(a::LazyProfileArray{1,T}, index::Int) where {T} = SVector{1,T}(a.g[index])
 
 function Base.show(io::IO, g::NormalFormGame)
     print(io, summary(g))
@@ -739,12 +739,10 @@ function Base.getindex(g::NormalFormGame{N,T},
     length(index) != N &&
         throw(DimensionMismatch("index must be of length $N"))
 
-    payoff_profile = Array{T}(undef, N)
-    for (i, player) in enumerate(g.players)
-        payoff_profile[i] =
-            player.payoff_array[(index[i:end]..., index[1:i-1]...)...]
-    end
-    return payoff_profile
+    return SVector{N,T}(ntuple(
+        i -> g.players[i].payoff_array[ntuple(k -> index[mod1(k+i-1, N)], Val(N))...],
+        Val(N),
+    ))
 end
 
 # Trivial game with 1 player
